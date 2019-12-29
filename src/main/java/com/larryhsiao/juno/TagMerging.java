691ce47dbbf141ce4cf4a3e544c830ce62664733
Larry Hsiao <larryhsiao@larryhsiao.com>
2019-12-30 00:51:05 +0800

    TagMerging

---
+++ src/main/java/com/larryhsiao/juno/TagMerging.java
@@ -0,0 +1,79 @@
+package com.larryhsiao.juno;
+
+import com.silverhetch.clotho.Action;
+import com.silverhetch.clotho.Source;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Collection;
+
+/**
+ * Action to merge given tags.
+ */
+public class TagMerging implements Action {
+    private final Source<Connection> db;
+    private final Tag[] tags;
+
+    public TagMerging(Source<Connection> db, Tag... tags) {
+        this.db = db;
+        this.tags = tags;
+    }
+
+    @Override
+    public void fire() {
+        try {
+            if (tags.length < 2) {
+                throw new IllegalArgumentException(
+                    "Tags should more then 2, given: " + tags.length
+                );
+            }
+            final Connection conn = db.value();
+            final String tagIds = tagIds();
+            final String fileIds = fileIds();
+            try (PreparedStatement stmt = conn.prepareStatement(
+                // language=H2
+                "UPDATE FILE_TAG SET TAG_ID=? " +
+                    "WHERE TAG_ID IN (" + tagIds + ") " +
+                    "AND FILE_ID NOT IN (" + fileIds + ");"
+            )) {
+                stmt.setLong(1, tags[0].id());
+                stmt.execute();
+            }
+            try (PreparedStatement stmt = conn.prepareStatement(
+                // language=H2
+                "DELETE FROM TAGS WHERE ID IN (" + tagIds + ")"
+            )) {
+                stmt.execute();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String fileIds() {
+        Collection<AFile> files = new QueriedAFiles(
+            new FilesByTagId(db, tags[0].id())
+        ).value().values();
+        final StringBuilder builder = new StringBuilder();
+        int i = 0;
+        for (AFile file : files) {
+            if (i > 1) {
+                builder.append(", ");
+            }
+            builder.append(file.id());
+            i++;
+        }
+        return builder.toString();
+    }
+
+    private String tagIds() {
+        final StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < tags.length; i++) {
+            if (i > 1) {
+                builder.append(", ");
+            }
+            builder.append(tags[i].id());
+        }
+        return builder.toString();
+    }
+}
---
+++ src/test/java/com/larryhsiao/juno/TagMergingTest.java
@@ -0,0 +1,39 @@
+package com.larryhsiao.juno;
+
+import com.larryhsiao.juno.h2.MemoryH2Conn;
+import com.silverhetch.clotho.Source;
+import com.silverhetch.clotho.database.SingleConn;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+/**
+ * Unit-test for the class {@link TagMerging}
+ */
+class TagMergingTest {
+
+    /**
+     * Check the output
+     */
+    @Test
+    void simple() {
+        final Source<Connection> db = new SingleConn(
+            new FakeDataConn(new TagDbConn(new MemoryH2Conn()))
+        );
+        new TagMerging(db,
+            new TagById(db, 1).value(),
+            new TagById(db, 2).value()
+        ).fire();
+
+        Assertions.assertEquals(1, new QueriedTags(
+            new AllTags(db)).value().size()
+        );
+        Assertions.assertEquals(
+            1,
+            new QueriedTags(
+                new TagsByFileId(db, 1)
+            ).value().size()
+        );
+    }
+}
